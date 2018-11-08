a80cee8223c87739dc5c366b140919f4ba20784c
Adhithya Srinivasan <adhithyasrinivasan@gmail.com>
2018-11-08 18:17:16 +0530

    Update DectoHex.java

--- DectoHex.java
+++ DectoHex.java
@@ -2,8 +2,9 @@
 public class DectoHex {
 public static void main(String[] args) {
 	
-	for(int i=11000;i<1100000;i++)
-		convertToHex(i);
+	
+	convertToHex(500);
+	convertToBin(500);
 	
 }
 static void convertToHex(int n)
@@ -23,5 +24,17 @@ static void convertToHex(int n)
 	}
 	System.out.println(rem);
 }
+static void convertToBin(int n)
+{
+	String rem="";
+	while(n>0)
+	{
+		int remn=n%2;
+		rem=remn+rem;
+		n=n/2;
+	}
+	System.out.println(rem);
+}
+
 
 }
